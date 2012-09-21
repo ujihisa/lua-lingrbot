e7d371f1bd4fe8ebb6940f618edfb688b79e0796
ujihisa <ujihisa@gmail.com>
2012-09-20 20:10:50 -0700

    omg

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ git pull --rebase
 cp -p ./index.cgi ~/public_html/cgi-bin/lua-lingrbot.cgi
 
 cd ~/public_html/cgi
-if [! -e underscore.lua ]; then
+if [ ! -e underscore.lua ]; then
   wget 'https://raw.github.com/mirven/underscore.lua/0.4-0/lib/underscore.lua'
 fi
